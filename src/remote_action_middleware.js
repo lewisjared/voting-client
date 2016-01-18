208b66c0f8860d8b12a65f617a8fe383e8afe235
Jared Lewis <jared@jared.kiwi.nz>
2016-01-18 13:35:20 +1300

    Added clientId to the Vote message

--- src/remote_action_middleware.js
+++ src/remote_action_middleware.js
@@ -1,6 +1,11 @@
+import clone from 'lodash/clone'
+
 export default socket => store => next => action => {
   if (action.meta && action.meta.remote) {
-    socket.emit('action', action);
+    const clientId = store.getState().get('clientId');
+    let clonedAction = clone(action);
+    clonedAction.clientId = clientId;
+    socket.emit('action', clonedAction);
   }
   return next(action);
 }
