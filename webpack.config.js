03085778fb0775d322ba735832e75dcacb31fc23
lewisjared <jared@jared.kiwi.nz>
2016-02-23 21:08:16 +1300

    support html5 urls

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,8 @@ module.exports = {
   ],
   devServer: {
     contentBase: './dist',
-    hot: true
+    hot: true,
+    historyApiFallback: true
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
